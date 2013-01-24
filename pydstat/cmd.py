50bb00b5847097831de128222f541b91d312e778
BEKIM DISHA <bekimdisha@gmail.com>
2013-01-24 22:22:16 +0100

    STORM-5775 - Modified pydstat.py to return the percentage of the CPU usage for all or specific process in a 1 second interval/screeshot
    STORM-5775 - Modified pydstat.py to return the percentage of the CPU usage for all or specific process in a 1 second interval/screeshot
    
    STORM-5775 - Modified pydstat.py to comply with the coding style guide. Modified setup.py to update the patch version number to 1.0.2 .
    
    STORM-5775. The interval for calculating the CPU usage of a certain process in pydstat.py is now a variable and not a constant. Is 1 by default.
    
    STORM-5775 - Modified pydstat.py and cmd.py of pydstat library. Now cmd.py can take three options for which proccess id the user want to log, what the screenshot time interval can be and the frequency of producing the logs.
    
    STORM-5775 - Modified pydstat.py and cmd.py of pydstat library. Got rid of shlex, not neccessary for when arguments are escaped.
    
    Bumping version of pydstat into 1.0.3.
    
    PEP8 driven changes made to cmd.py and pydstat.py
    
    Pydstat error handling change to allow cron to run the script with defaults without showing help menu. Bumped the pydstat version into 1.1.0.

--- pydstat/cmd.py
+++ pydstat/cmd.py
@@ -8,10 +8,9 @@ __author__ = 'Greg Albrecht <gba@splunk.com>'
 __copyright__ = 'Copyright 2012 Splunk, Inc.'
 __license__ = 'Apache License 2.0'
 
-
-import sys
-
+import optparse
 import pydstat
+import sys
 
 
 def main():
@@ -20,16 +19,55 @@ def main():
     If you don't specify a PID we'll stat them all.
     If you specify a PID, we'll just stat that one.
     """
+    proc_id = None
+    interval = None
+    frequency = None
+
+    #explains the use of this option/argument parser
+    use = "Usage: %prog [options] processID interval frequency"
+
+    parser = optparse.OptionParser(usage=use)
+
+    parser.add_option('-p',
+                      '--proc_id',
+                      dest='proc_id',
+                      help='Process ID')
+
+    parser.add_option('-i',
+                      '--interval',
+                      dest='interval',
+                      help='Time interval of screenshot.',
+                      type=int)
+
+    parser.add_option('-f',
+                      '--frequency',
+                      dest='frequency',
+                      help='Times the script will run. 1 by default',
+                      type=int)
+
+    (options, args) = parser.parse_args()
+
+    proc_id = options.proc_id
+    if proc_id is None:
+        proc_id = "ALL"
+
+    interval = options.interval
+    if interval is None:
+        interval = 1
+
+    frequency = options.frequency
+    if frequency is None:
+        frequency = 1
+
+    #create object
     pyds = pydstat.PydStat()
 
-    if len(sys.argv) != 2:
-        pyds.get_stats()
-    else:
-        pyds.get_stats(sys.argv[1])
+    #Get Statistics. Three arguments : process ID, interval and frequency
+    pyds.get_stats(proc_id, interval, frequency)
 
+    #parse and log statistics
     pyds.parse_stats()
     pyds.log_stats()
-
 
 if __name__ == '__main__':
     sys.exit(main())
